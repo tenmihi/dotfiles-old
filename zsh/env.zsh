d23a59a683c47194704cd59933879563d9bad77a
tenmihi <tenmihi@gmail.com>
2017-07-11 22:55:03 +0900

    fix(env.zsh): fix env export path

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,15 +1,15 @@
 #! /bin/zsh
 
 # plenv
+export PLENV_ROOT=$HOME/.plenv
+export PATH=$PLENV_ROOT/bin:$PATH
 if [ -f 'plenv' ]; then
-  export PLENV_ROOT=$HOME/.plenv
-  export PATH=$PLENV_ROOT/bin:$PATH
   eval "$(plenv init -)"
 fi
 
 # ndenv
+export PATH="$HOME/.ndenv/bin:$PATH"
 if [ -f 'ndenv' ]; then
-  export PATH="$HOME/.ndenv/bin:$PATH"
   eval "$(ndenv init -)"
 fi
 
