55456a90921c53c09ec1d10877e2a07c303c5786
murao <murao@mfpc305.local>
2017-07-13 11:20:55 +0900

    chore(alias): move denco aliases to mf_config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,7 +1,3 @@
-#
-# aliases
-#
-
 # git add
 alias gaa='git add -A'
 
@@ -18,8 +14,3 @@ fi
 
 # carton exec
 alias carton-exec='carton exec -- plackup -Ilib -R ./lib --access-log /dev/null -p5000 -a'
-
-# Denco
-alias denco-plack-log='tail -F $MF_DEV_HOME/var/log/plack/denco.error.log'
-alias denco-diff-test='./script/dev_env.sh carton exec prove t/db/fixture/different_tables.t'
-alias denco-story-test='./script/dev_env.sh carton exec prove t/db/fixture/story_body.t'
--- zsh/mf_config.zsh
+++ zsh/mf_config.zsh
@@ -11,9 +11,14 @@ fi
 
 # anyenv
 export PATH="$HOME/.anyenv/bin:$PATH"
-if [ -f 'anyenv' ]; then
+if builtin command -v anyenv > /dev/null ; then
   eval "$(anyenv init -)"
 fi
 
 # fix git log multi byte character issue
 export LESSCHARSET=utf-8
+
+# Denco
+alias denco-plack-log='tail -F $MF_DEV_HOME/var/log/plack/denco.error.log'
+alias denco-diff-test='./script/dev_env.sh carton exec prove t/db/fixture/different_tables.t'
+alias denco-story-test='./script/dev_env.sh carton exec prove t/db/fixture/story_body.t'
