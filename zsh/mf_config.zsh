852aaad26a75d1249266a1cb78a510bc6cb56cc9
tenmihi <tenmihi@gmail.com>
2017-07-25 09:49:44 +0900

    feat(mf_config.zsh): add denco plack restart alias

--- zsh/mf_config.zsh
+++ zsh/mf_config.zsh
@@ -4,9 +4,8 @@ compinit
 promptinit
 
 # safe rm
-if [ -d ${HOME}/.Trash ]
-then
-    alias rm='mv --backup=numbered --target-directory=${HOME}/.Trash'
+if [ -d $HOME/.Trash ]; then
+  alias rm='mv --backup=numbered --target-directory=${HOME}/.Trash'
 fi
 
 # anyenv
@@ -18,7 +17,14 @@ fi
 # fix git log multi byte character issue
 export LESSCHARSET=utf-8
 
-# Denco
+# alias
 alias denco-plack-log='tail -F $MF_DEV_HOME/var/log/plack/denco.error.log'
 alias denco-diff-test='./script/dev_env.sh carton exec prove t/db/fixture/different_tables.t'
 alias denco-story-test='./script/dev_env.sh carton exec prove t/db/fixture/story_body.t'
+alias denco-prove='./script/dev_env.sh carton exec prove'
+alias denco-restart="$HOME/project/Denco/script/restart-server"
+
+# rmate
+if [ -f $HOME/local/bin/rmate ]; then
+  export PATH="$HOME/local/bin:$PATH"
+fi
