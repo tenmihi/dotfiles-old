b8b9760bc0550083b3d08f961f49ba3d3642412e
tenmihi <tenmihi@gmail.com>
2017-07-25 23:03:40 +0900

    feat(.files): dotfiles in repository no longer hidden

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-dot_files=(".vimrc" ".tmux.conf" ".zshrc")
+dot_files=("vimrc" "tmux.conf" "zshrc")
 
 for file in $dot_files;
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+    ln -s $HOME/dotfiles/$file $HOME/.$file
 done
