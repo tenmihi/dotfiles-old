635a9c5125b19399777f04df058781683ac30467
tenmihi <tenmihi@gmail.com>
2018-11-01 10:53:00 +0900

    master chore(alias): add some alises

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -17,3 +17,14 @@ alias push='git push'
 if builtin command -v rmtrash > /dev/null ; then
   alias rm='rmtrash'
 fi
+
+# copy to clipboard
+case ${OSTYPE} in
+    linux*)
+    alias pbcopy='xsel --clipboard --input'
+    alias pbpaste='xsel --clipboard --output'
+    ;;
+esac
+
+# file search & move
+alias fc='cd "$(find . -type d | peco)"'
